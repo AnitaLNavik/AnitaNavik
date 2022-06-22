0006b5d506db71346dfc4bdab2a54f03815bd9be
AnitaLNavik <107745440+AnitaLNavik@users.noreply.github.com>
2022-06-22 11:30:33 +0530

    Add files via upload

---
+++ practical/anudip.java
@@ -0,0 +1,10 @@
+class anpudip{
+public static void main(String bhaskar[])
+{
+int firstNum=5;
+int secondNum=10;
+System.out.println("first number is :''+firstNum+"and second number: 
+is "+secondNum);
+
+}
+}
